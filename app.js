ed56d042321533195ac395b8088f395012ed42ca
francesco <franclombardo@gmail.com>
2014-10-07 10:16:05 +0200

    first

---
+++ app.js
@@ -0,0 +1,112 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var io = require('socket.io')
+var spawn = require('child_process').spawn;
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(cookieParser());
+//app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
+var debug = require('debug')('webconsole');
+var sh; // = spawn("sh");
+
+app.set('port', process.env.PORT || 3000);
+
+var server = app.listen(app.get('port'), function() {
+    debug('Express server listening on port ' + server.address().port);
+});
+
+io = io.listen(server, {
+    log: true
+});
+console.log("stocazzo");
+io.sockets.on('connection', function(socket) {
+    console.log('onConnection');
+    sh = spawn("/bin/sh");
+    console.log(sh.connected);
+    sh.stdout.setEncoding('utf-8');
+    sh.stdin.setEncoding('utf-8');
+    sh.stdout.on('data', function(data) {
+        console.log('data ' + data)
+        socket.emit('cmd_res', data);
+    });
+    sh.stderr.setEncoding('utf-8');
+    sh.stderr.on('data', function(data) {
+        console.log('dataerr ' + data)
+        socket.emit('cmd_res', data);
+    });
+    console.log(sh.connected);
+
+    socket.on('disconnect', function(data) {
+        console.log('disconnesso: onDisconnect');
+    });
+
+
+    socket.on('cmd', function(data) {
+        console.log('cmd: ' + data);
+        console.log(sh.connected);
+        sh.stdin.write(data + "\n");
+
+    });
+
+
+});
+
+
+
+module.exports = app;
---
+++ public/javascripts/fwebconsole.js
@@ -0,0 +1,151 @@
+if (typeof dreamer === 'undefined') {
+    var dreamer = {};
+}
+
+dreamer.Fwc = (function(global) {
+    'use strict';
+    var timeout = 5000;
+    var ws;
+    var cmdHistory = [];
+    var _channel;
+    var _div;
+    var _termoutput;
+    var _cmd;
+    var _clipboard;
+
+    function Fwc(div, channel) {
+        _div = div;
+        _channel = channel;
+        _termoutput = "terminal-output-" + _channel;
+        _cmd = "cmd-" + _channel;
+        _clipboard = "clipboard-" + _channel;
+        initTerminalOutput();
+        initPromtLine();
+        initWebSocket();
+        initListeners();
+
+    }
+
+    function sendCmd(cmd, invisible) {
+        if (cmd == 'clear') {
+            //TODO aggiungere righe vuote e scalare
+            //$('#log').html('');
+            return;
+        }
+        try {
+
+            if(invisible == undefined || invisible == false){
+              cmdHistory.push(cmd);
+              $('#' + _termoutput).append("<div style=\"width: 100%; visibility: visible;\"> >  " + cmd + "</div>");
+            }
+            ws.emit('cmd', cmd)
+        } catch (err) {
+            console.log(err);
+
+        }
+    }
+
+    function initPromtLine() {
+        $(_div).append("<div class=\"cmd\" id=\"" + _cmd + "\" style=\"width: 100%; visibility: visible;\"><span class=\"prompt\">&gt;&nbsp;</span><span></span><span class=\"cursor blink\">&nbsp;</span><span></span><textarea class='clipboard' id=\"" + _clipboard + "\" ></textarea></div>");
+    }
+
+    function initTerminalOutput() {
+        $(_div).append("<div class='terminal-output' id=\"" + _termoutput + "\"></div>");
+        
+    }
+
+    function initListeners() {
+        
+        $("#" + _clipboard).keypress(function(e) {
+            console.log(e);
+            if (e.keyCode != 13) {
+
+
+                return;
+            }
+            else if(e.ctrlKey==true && e.charCode == 99){
+                console.log("CTRL+C")
+                sendCtrC();
+            }
+            else{
+              var cmd = $("#" + _clipboard).attr("value").replace("\n", "");
+              if (!isBlank(cmd)) {
+                sendCmd(cmd);
+              }
+              $("#" + _clipboard).attr("value", "");
+            }
+            
+        });
+        $("#" + _clipboard).bind('copy', function(e) {
+            console.log("CTRL+C")
+            e.preventDefault();
+            sendCtrC();
+        });
+        $("#" + _clipboard).bind('cut', function(e) {
+            console.log("CTRL+X")
+            e.preventDefault();
+        });
+        $("#" + _clipboard).bind('undo', function(e) {
+            console.log("CTRL+Z");
+            e.preventDefault();
+        });
+        $(_div).click(function(e) {
+            $("#" + _clipboard).focus();
+            return false;
+        }); 
+
+        
+        
+    }
+    function sendCtrC(){
+        sendCmd("\\x03", true);
+    }
+    function toStaticHTML(data) {
+        data = data.toString();
+        return data.replace(/&/g, "&amp;")
+            .replace(/</g, "&lt;")
+            .replace("/n", "<br/>")
+            .replace(/>/g, "&gt;");
+    }
+
+    function isBlank(text) {
+        var blank = /^\s*$/;
+        return (text.match(blank) !== null);
+    }
+
+    function initWebSocket() {
+        ws = io.connect('/');
+        ws.on('connect', function() {
+            console.log('connected to server');
+        });
+        ws.on('cmd_res', function(data) {
+
+            var rows = data.split('\n');
+            for (var r in rows) {
+                console.log(rows[r]);
+                $("#" + _termoutput).append("<div style=\"width: 100%; visibility: visible;\">" + toStaticHTML(rows[r]) + "</div>");
+            }
+
+            window.scrollBy(0, 100000000000000000);
+        });
+        ws.on('disconnect', function() {
+            //TODO
+        });
+    }
+
+    Fwc.prototype.prova = function() {
+
+    };
+
+
+    return Fwc;
+}());
+
+if (typeof module === 'object') {
+    module.exports = dreamer.Fwc;
+}
+
+
+$(document).ready(function() {
+    var prova = new dreamer.Fwc('#wfconsole', 'prova');
+});
