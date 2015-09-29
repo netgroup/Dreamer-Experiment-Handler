d119297b8f8d9191d0f4625167097919f2ddd56f
Your Name <you@example.com>
2015-09-29 18:13:24 +0200

    fix path

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ var config = {}
 
 config.mininet = {};
 
-config.mininet.mininet_extension_path = "/home/user/my_workspace/Dreamer-Mininet-Extensions";
+config.mininet.mininet_extension_path = "/home/user/workspace/Dreamer-Mininet-Extensions";
 
 module.exports = config;
